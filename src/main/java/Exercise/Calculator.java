6e7772345d7d6d1e6c1a0ce4ea68d223c69deedd
Caz77z <112615422+Caz77z@users.noreply.github.com>
2022-09-02 08:52:18 +0200

    Lagt till Calculator

---
+++ src/main/java/Exercise/Calculator.java
@@ -0,0 +1,19 @@
+package Exercise;
+import java.util.stream.DoubleStream;
+
+public class Calculator {
+
+    static double add(double... operands) {
+        return DoubleStream.of(operands)
+                .sum();
+    }
+
+    static double multiply(double... operands) {
+        return DoubleStream.of(operands)
+                .reduce(1, (a, b) -> a * b);
+    }
+
+    public int getSum(int i, int j) {
+        return i+j;
+    }
+}
